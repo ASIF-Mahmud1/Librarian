b1a1cab72b5de4e89cc227b38a0630e14acac82a
Your Name <you@example.com>
2018-02-12 23:35:20 +0600

    Hopefully Done

--- librarian.js
+++ librarian.js
@@ -22,21 +22,66 @@ function addBook( book ){
   return library;
 }
 
-var newBook = {title: "The ", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf  "};
+var newBook = {title: "My Auto Biography ", year: "2018", author: "Asif Mahmud", publisher: "Noone is Interested to publish My Book"};
 console.log(addBook(newBook));
 
 /* Task 4 */
 function bookByAuthor(){
-  //add code
+  var author_name=[];
+  var temp=[];
+
+  for(var i=0; i< library.length; i++)
+  {
+    author_name[i]= library[i].author;
+  }
+  author_name.sort();
+
+  for(var i=0; i< library.length; i++)
+  {
+    for(var j=0; j<library.length; j++)
+    {
+    if(author_name[i]==library[j].author)
+
+    {
+    //  temp[i].title= library[j].title;
+      //temp[i].year= library[j].year;
+      //temp[i].author=library[j].author;
+      //temp[i].publisher= library[j].publisher;
+      temp[i]=library[j];
+    }
+   }
+  }
+  for(var i=0; i< library.length; i++)
+  {
+    //library[i].title= temp[i].title;
+  ///  library[i].year= temp[i].year;
+  //  library[i].author=temp[i].author;
+  //  library[i].publisher= temp[i].publisher;
+  library[i]=temp[i];
+  }
+  return library;
 }
 
 console.log(bookByAuthor());
 
 /* Task 5 */
 function findByTitle( title ){
-  //add code
+  var lib2=[]; var counter=0;
+
+  for(var i=0; i<library.length;i++)
+  {
+    var str= library[i].title;
+
+    if(str.toLowerCase().indexOf(title)!= -1)
+    {
+      lib2[counter]= library[i];
+      counter++;
+    }
+  }
+  return lib2;
 }
 
 
 console.log(findByTitle("man"));
 console.log(findByTitle("myth"));
+console.log(findByTitle("auto"));
